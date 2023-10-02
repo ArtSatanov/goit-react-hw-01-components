a1c7391dd4e0b222b39fbb62db64694d7a0e657a
ArtSatanov <artsatanov@gmail.com>
2023-10-03 02:27:40 +0300

    task1

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -5,7 +5,7 @@ import {Stat} from "./stat/Stat"
 
 export const Profile = ({ user }) => { 
    return (
-      <div className="profile">
+      <div className={styles.profile}>
       <Avatar avatar={user.avatar} username={user.username} tag={user.tag} location={user.location} />
       <Stat followers={user.stats.followers} views={user.stats.views} likes={user.stats.likes}/>
       </div>
