949b233dc09e56f196cde7a2972fc4ee8ff7a151
ArtSatanov <artsatanov@gmail.com>
2023-10-04 00:37:52 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { FriendList } from "./friend-list/FriendList";
 import profile from "./profile/assets/user.json";
 import statistics from "./statistics/assets/data.json"
 import friends from "./friend-list/assets/friends.json"
+import transactions from "./transactions/assets/transactions.json"
 
 
 
@@ -16,6 +17,7 @@ export const App = () => {
       <Profile user={profile} />
       <Statistics data={statistics} />
       <FriendList friends={friends} />
+      <TransactionHistory items={ transactions} />
       
     </div>
   );
