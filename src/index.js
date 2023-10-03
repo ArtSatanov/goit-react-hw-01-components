051252c9717a594a1100263d80aa54d733938eca
ArtSatanov <artsatanov@gmail.com>
2023-10-03 14:30:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import data from "./components/profile/assets/user.json";
-import statistics from "./components/Statistics/assets/data.json"
+import users from "./components/profile/assets/user.json";
+// import statistics from "./components/Statistics/assets/data.json"
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App data={data}/>
+    <App data={users}/>
   </React.StrictMode>
 );
